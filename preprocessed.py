72b24f7257ce4003ee59c7d2c2e8c85b23bf4147
unknown <hesam.jalalian@yahoo.com>
2022-01-12 12:26:03 -0500

    updated

--- preprocessed.py
+++ preprocessed.py
@@ -1,60 +1,66 @@
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from scipy.io import loadmat, savemat
 from sklearn import preprocessing
 from sklearn.impute import SimpleImputer
 import glob
-from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit, train_test_split, KFold
+from sklearn.preprocessing import StandardScaler
 
-# for file in glob.glob("dataset/*.*"):
 for file in glob.glob("datasets/*.*"):
+    print(file)
+
     input = loadmat(file)
     data = input['dataset']
 
     X = data[:, 0:-1]
     y = data[:, -1]
+    # print(y)
 
-    sss = StratifiedShuffleSplit(n_splits=20, test_size=0.25, random_state=1000)
-    sss.get_n_splits(X, y)
+    kf = KFold(n_splits=10)
+
+    # add cross validation for 10 fold
+    kf.sss = StratifiedShuffleSplit(n_splits=20, test_size=0.25, random_state=1000)
+    kf.sss.get_n_splits(X, y)
     split_part=0
     name = file.split(".")[0].split("\\")[1]
 
-    for train_index, test_index in sss.split(X, y):
+    for train_index, test_index in kf.sss.split(X, y):
         split_part=split_part+1
         #print("TRAIN:", train_index, "TEST:", test_index)
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-        label_encoder = preprocessing.LabelEncoder()
-        y_train_ms = label_encoder.fit_transform(y_train)
-        y_test_ms = label_encoder.transform(y_test)
-
-        x_train_1, x_dsel, y_train_1, y_dsel = train_test_split(X_train, y_train_ms, test_size=0.33,random_state=42)
-        print(len(x_train_1))
-        print(len(X_test))
-        print(len(x_dsel))
-        print(len(y_train_1))
-        print(len(y_test_ms))
-        print(len(y_dsel))
-
+        # standard scaler
         scaler = StandardScaler()
-        X_train_ss = scaler.fit_transform(x_train_1)
+        X_train_ss = scaler.fit_transform(X_train)
         X_test_ss = scaler.transform(X_test)
-        X_dsel_ss = scaler.transform(x_dsel)
 
+        # handling the missing data and replace missing values with mean of all the other values
         imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
         imputer = imputer.fit(X_train_ss)
-        imputer = imputer.fit(X_dsel_ss)
         X_train_ms = imputer.transform(X_train_ss)
         X_test_ms = imputer.transform(X_test_ss)
-        X_dsel_ms = imputer.transform(X_dsel_ss)
 
-        X_train_processed = X_train_ms
+        # , stratify = y_train
+        x_train_f, x_dsel, y_train_f, y_dsel = train_test_split(X_train_ms, y_train, test_size=0.33, stratify=y_train)
+        # print(len(x_train_f))
+        # print(len(X_test_ms))
+        # print(len(x_dsel))
+        # print(len(y_train_f))
+        # print(len(y_test))
+        # print(len(y_dsel))
+
+        label_encoder = preprocessing.LabelEncoder()
+        y_train_ms = label_encoder.fit_transform(y_train_f)
+        y_test_ms = label_encoder.transform(y_test)
+        y_dsel_ms = label_encoder.transform(y_dsel)
+
+        X_train_processed = x_train_f
         X_test_processed = X_test_ms
-        X_dsel_processed = X_dsel_ms
-        y_train_processed = y_train_1
+        X_dsel_processed = x_dsel
+        y_train_processed = y_train_ms
         y_test_processed = y_test_ms
-        y_dsel_processed = y_dsel
+        y_dsel_processed = y_dsel_ms
 
         processed = [X_train_processed, X_test_processed, y_train_processed, y_test_processed, X_dsel_processed, y_dsel_processed]
         # processed = [X_train_processed, y_train_processed]
@@ -67,8 +73,4 @@ for file in glob.glob("datasets/*.*"):
         for i in range(len(processed)):
             FramStack[i]=processed[i]
 
-
         np.save('processed_8/{}_dir/processed_{}_{}'.format(name, name, split_part), {"FrameStack": FramStack})
-
-
-
